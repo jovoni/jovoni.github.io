8a4c940fe8cc987aab4c453b7cd97139444b18e0
jovoni <giovy.santa@gmail.com>
2021-11-08 09:45:15 +0100

    Added books banner

--- script.js
+++ script.js
@@ -24,9 +24,31 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing", {
-        strings: ["pirla"],
+        strings: ["un uomo", "un papà", "italiano", "cristiano"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
     });
 });
