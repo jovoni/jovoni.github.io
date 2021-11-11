c1fc8eadaae41fd87e1bc2ffa42d6327388af976
jovoni <giovy.santa@gmail.com>
2021-11-11 13:28:08 +0100

    mc

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing", {
-        strings: ["un armadillo", "italiano", "cristiano"],
+        strings: ["parolaio", "solista", "bambino"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
