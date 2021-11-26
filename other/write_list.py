9212c170fcab43c05b506067156f467299e11068
jovoni <giovy.santa@gmail.com>
2021-11-26 13:48:59 +0100

    mc

--- other/write_list.py
+++ other/write_list.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import sys
 
-def write_book(titolo, autore, link, is_special):
+def write_item(titolo, autore, link, is_special):
     if not is_special:
         res = f'<div class="item"> <a href="{link}">{titolo}</a> <span>di {autore}</span> </div>'
     else: 
@@ -11,21 +11,47 @@ def write_book(titolo, autore, link, is_special):
 
 if __name__ == "__main__":
     
-    csv = sys.argv[1]
-    file_name = sys.argv[2]
+    csv_names = ["album.csv", "visioni.csv", "letture.csv", "serietv.csv", "singoli.csv"]
+    txt_names = ["album.txt", "visioni.txt", "letture.txt", "serietv.txt", "singoli.txt"]
     
-    df = pd.read_csv(csv)
     
-    with open(file_name, "w") as f:
-        for i in range(len(df)):
-            print(i)
-            titolo = (df["titolo"][i])
-            autore = (df["autore"][i])
-            link = (df["link"][i])
-            is_special = (df["special"][i])
+    for j, csv in enumerate(csv_names):
+        df = pd.read_csv(csv)
+        print(csv)
+        file_name = txt_names[j]
+        
+        with open(file_name, "w") as f:
+            print(file_name)
             
-            line = write_book(titolo,autore,link,is_special)
-            f.write(line)
-            f.write("\n")
+            for i in range(len(df)):
+            
+                titolo = (df["titolo"][i])
+                autore = (df["autore"][i])
+                link = (df["link"][i])
+                is_special = (df["special"][i])
+                
+                line = write_item(titolo,autore,link,is_special)
+                f.write(line)
+                f.write("\n")
+            
+        
+        
+    
+    # csv = sys.argv[1]
+    # file_name = sys.argv[2]
+    
+    # df = pd.read_csv(csv)
+    
+    # with open(file_name, "w") as f:
+    #     for i in range(len(df)):
+    #         print(i)
+    #         titolo = (df["titolo"][i])
+    #         autore = (df["autore"][i])
+    #         link = (df["link"][i])
+    #         is_special = (df["special"][i])
+            
+    #         line = write_item(titolo,autore,link,is_special)
+    #         f.write(line)
+    #         f.write("\n")
         
         
